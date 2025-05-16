a1a75ff4efb8642779b37d484ac94a8558b79bac
amyme123 <amy-m-elamblasserial@ibm.com>
2025-05-16 10:16:19 +0530

    commit

--- src/components/BottomGrid.js
+++ src/components/BottomGrid.js
@@ -1,5 +1,4 @@
-// src/components/BottomGrid.js
-import React from 'react';
+
 
 const BottomGrid = () => {
   const cards = [
--- src/components/FeatureGrid.js
+++ src/components/FeatureGrid.js
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import pic2 from '../assets/pic2.jpg';
 import pic3 from '../assets/pic3.jpg';
 import pic4 from '../assets/pic4.jpg';
